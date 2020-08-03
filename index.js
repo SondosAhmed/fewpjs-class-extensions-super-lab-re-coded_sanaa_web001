1a92081286dc46220600cceda425276fda8e2444
Sondos Ahmed <sondosalshami@gmail.com>
2020-08-03 19:24:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ class Tree{
   constructor(species){
     this.species=species;
   }
+  static definition(){
+    return "A tree is a perennial plant with an elongated stem, or trunk, supporting";
+
+  }
 }
 class Deciduous extends Tree{
   constructor(species, name){
